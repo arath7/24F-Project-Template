563b9feb3e1ba22a4f4abf651b987f0a05196624
Ananya Rath <rath.ananya@gmail.com>
2024-12-05 15:32:38 -0500

    stats page done and created advisor home page

--- app/src/pages/statistics.py
+++ app/src/pages/statistics.py
@@ -1,6 +1,7 @@
 import requests
 import streamlit as st
 from modules.nav import SideBarLinks
+import pandas as pd
 
 # Page configuration
 st.set_page_config(page_title="Statistics", layout="wide")
@@ -14,11 +15,7 @@ routes = {
     'total_reviews': 'http://api:4000/a/admin/total_reviews',
     'total_employers': 'http://api:4000/a/admin/total_employers',
     'jobs_by_category': 'http://api:4000/a/admin/jobs_by_category'
-
-
 }
-
-
 
 
 stats = {}
@@ -33,26 +30,32 @@ for key, route in routes.items():
 # Display statistics
 st.title("Statistics")
 
-st.header("Return Offer Percentage")
-return_offer_percentage = stats.get('return_offer_percentage', {})
-st.write(return_offer_percentage)
+return_offer_percentage = stats.get('return_offer_percentage', {}).get('returnOfferPercentage')
+total_students = (stats.get('total_students', []))[0]["COUNT(NUID)"]
+total_jobs = (stats.get('total_jobs', []))[0]["COUNT(JobID)"]
+total_reviews = (stats.get('total_reviews', []))[0]["COUNT(reviewID)"]
+total_employers = (stats.get('total_employers', []))[0]["COUNT(employerID)"]
 
-st.header("Total Students")
-total_students = stats.get('total_students', {})
-st.write(total_students)
 
-st.header("Total Jobs")
-total_jobs = stats.get('total_jobs', {})
-st.write(total_jobs)
+st.markdown("### Application Statistics")
+col1, col2, col3, col4, col5 = st.columns(5)
+with col1:
+    st.metric(label="Return Offer Percentage", value=return_offer_percentage)
+with col2:
+    st.metric(label="Total Students", value=total_students)
+with col3:
+    st.metric(label="Total Jobs", value=total_jobs)
+with col4:
+    st.metric(label="Total Reviews", value=total_reviews)    
+with col5:
+    st.metric(label="Total Employers", value=total_employers)
 
-st.header("Total Reviews")
-total_reviews = stats.get('total_reviews', {})
-st.write(total_reviews)
 
-st.header("Total Employers")
-total_employers = stats.get('total_employers', {})
-st.write(total_employers)
 
-st.header("Jobs by Category")
-jobs_by_category = stats.get('jobs_by_category', {})
-st.write(jobs_by_category)
+jobs_by_cat = stats.get('jobs_by_category', [])
+
+df = pd.DataFrame(jobs_by_cat)
+df.rename(columns={"COUNT(j.JobID)": "Number of Jobs", "Name": "Category"}, inplace=True)
+df = df[["Category", "Number of Jobs"]]
+st.markdown("### Overview of Job Categories")
+st.dataframe(df, use_container_width=True)
