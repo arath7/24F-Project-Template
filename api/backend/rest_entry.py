4f23d02b75e1b428b0098bf2d0772c7d7f1849af
Ananya Rath <rath.ananya@gmail.com>
2024-12-06 21:02:31 -0500

    bug

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
 from backend.db_connection import db
-from backend.customers.customer_routes import customers
-from backend.products.products_routes import products
-from backend.simple.simple_routes import simple_routes
 from backend.jobs_routes import jobs
 from backend.review_routes import review
 from backend.employer_routes import employer
@@ -48,9 +45,6 @@ def create_app():
     # Register the routes from each Blueprint with the app object
     # and give a url prefix to each
     app.logger.info('current_app(): registering blueprints with Flask app object.')   
-    app.register_blueprint(simple_routes)
-    app.register_blueprint(customers, url_prefix='/c')
-    app.register_blueprint(products, url_prefix='/p')
     app.register_blueprint(jobs, url_prefix='/j')
     app.register_blueprint(job_category, url_prefix='/jc')
     app.register_blueprint(review, url_prefix='/r')
